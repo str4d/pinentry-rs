3923f9039553bb592b777011f5f3110abd26a311
Jack Grigg <thestr4d@gmail.com>
2020-01-29 23:42:21 +0000

    Add title, error, and button configuration to PassphraseInput

--- src/lib.rs
+++ src/lib.rs
@@ -41,9 +41,13 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// A dialog for requesting a passphrase from the user.
 pub struct PassphraseInput<'a> {
     binary: PathBuf,
+    title: Option<&'a str>,
     description: Option<&'a str>,
+    error: Option<&'a str>,
     prompt: Option<&'a str>,
     confirmation: Option<(&'a str, &'a str)>,
+    ok: Option<&'a str>,
+    cancel: Option<&'a str>,
     timeout: Option<u16>,
 }
 
@@ -63,18 +67,42 @@ impl<'a> PassphraseInput<'a> {
     /// - A binary name was provided that cannot be found in `PATH`.
     /// - The binary is found but is not executable.
     pub fn with_binary<T: AsRef<OsStr>>(binary_name: T) -> Option<Self> {
-        which::which(binary_name).ok().map(|binary| PassphraseInput {
-            binary,
-            description: None,
-            prompt: None,
-            confirmation: None,
-            timeout: None,
-        })
+        which::which(binary_name)
+            .ok()
+            .map(|binary| PassphraseInput {
+                binary,
+                title: None,
+                description: None,
+                error: None,
+                prompt: None,
+                confirmation: None,
+                ok: None,
+                cancel: None,
+                timeout: None,
+            })
+    }
+
+    /// Sets the window title.
+    ///
+    /// When using this feature you should take care that the window is still identifiable
+    /// as the pinentry.
+    pub fn with_title(&mut self, title: &'a str) -> &mut Self {
+        self.title = Some(title);
+        self
     }
 
     /// Sets the descriptive text to display.
     pub fn with_description(&mut self, description: &'a str) -> &mut Self {
         self.description = Some(description);
+        self
+    }
+
+    /// Sets the error text to display.
+    ///
+    /// This is used to display an error message, for example on a second interaction if
+    /// the first passphrase was invalid.
+    pub fn with_error(&mut self, error: &'a str) -> &mut Self {
+        self.error = Some(error);
         self
     }
 
@@ -110,6 +138,29 @@ impl<'a> PassphraseInput<'a> {
         self
     }
 
+    /// Sets the text for the button signalling confirmation (the "OK" button).
+    ///
+    /// You should use an underscore in the text only if you know that a modern version of
+    /// pinentry is used. Modern versions underline the next character after the
+    /// underscore and use the first such underlined character as a keyboard accelerator.
+    /// Use a double underscore to escape an underscore.
+    pub fn with_ok(&mut self, ok: &'a str) -> &mut Self {
+        self.ok = Some(ok);
+        self
+    }
+
+    /// Sets the text for the button signaling cancellation or disagreement (the "Cancel"
+    /// button).
+    ///
+    /// You should use an underscore in the text only if you know that a modern version of
+    /// pinentry is used. Modern versions underline the next character after the
+    /// underscore and use the first such underlined character as a keyboard accelerator.
+    /// Use a double underscore to escape an underscore.
+    pub fn with_cancel(&mut self, cancel: &'a str) -> &mut Self {
+        self.cancel = Some(cancel);
+        self
+    }
+
     /// Sets the timeout (in seconds) before returning an error.
     pub fn with_timeout(&mut self, timeout: u16) -> &mut Self {
         self.timeout = Some(timeout);
@@ -122,11 +173,23 @@ impl<'a> PassphraseInput<'a> {
     pub fn interact(&self) -> Result<Option<SecretString>> {
         let mut pinentry = assuan::Connection::open(&self.binary)?;
 
+        if let Some(title) = &self.title {
+            pinentry.send_request("SETTITLE", Some(title))?;
+        }
         if let Some(desc) = &self.description {
             pinentry.send_request("SETDESC", Some(desc))?;
         }
+        if let Some(error) = &self.error {
+            pinentry.send_request("SETERROR", Some(error))?;
+        }
         if let Some(prompt) = &self.prompt {
             pinentry.send_request("SETPROMPT", Some(prompt))?;
+        }
+        if let Some(ok) = &self.ok {
+            pinentry.send_request("SETOK", Some(ok))?;
+        }
+        if let Some(cancel) = &self.cancel {
+            pinentry.send_request("SETCANCEL", Some(cancel))?;
         }
         if let Some((confirmation_prompt, mismatch_error)) = &self.confirmation {
             pinentry.send_request("SETREPEAT", Some(confirmation_prompt))?;
